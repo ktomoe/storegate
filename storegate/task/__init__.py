e75a4f66a377097ee227a84a559571e73bc73855
Tomoe Kishimoto <tomoe.kishimoto@kek.jp>
2026-03-03 07:19:51 +0000

    Updates

---
+++ storegate/task/__init__.py
@@ -0,0 +1,13 @@
+from storegate.task.task import Task
+from storegate.task.agent_task import AgentTask
+from storegate.task.dl_task import DLTask
+from storegate.task.dl_env import DLEnv
+from storegate.task.pytorch_task import PytorchTask
+
+__all__ = [
+    'Task',
+    'AgentTask',
+    'DLTask',
+    'DLEnv',
+    'PytorchTask',
+]
